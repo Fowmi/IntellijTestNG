975e8896e3f120dd318d800d8ac90fe5a4a93032
Fow <fowminazeer@gmail.com>
2023-08-11 11:12:17 +0530

    Added a function

---
+++ src/ThhJffKgg.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class ThhJffKgg
+{
+    @Test(priority = 2)
+    void hjj()
+    {System.out.println("gg hh");}
+    @Test(priority = 1)
+    void aa()
+    {System.out.println("klm");}
+    @Test
+            void findd()
+    {System.out.println("klm");}
+}
